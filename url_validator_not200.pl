cf558abe3feba56d1b033bb26f944d7e0f881bae
joginder <jogind3r@gmail.com>
2016-06-08 14:16:37 +0530

    follow link
    follow link

--- url_validator_not200.pl
+++ url_validator_not200.pl
@@ -4,7 +4,7 @@ if(!($_ = shift)=~/\w/){ print "please input a file\n";exit();}
 @lines = `cat $_`;
 for $i(0 .. $#lines){
 	$_=$lines[$i];chomp;
-	$x="curl -s -D - $_ -o /dev/null";
+	$x="curl -sL -D - $_ -o /dev/null";
 	$o=`$x | head -n 1`;
 	if( !($o=~/200/)){
 		print $i+1 . ": $_ <> !200 found \n";

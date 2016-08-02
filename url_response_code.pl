e3eebd27d7e1fd263a2bbe3870d66c1a823c44ce
joginder <jogind3r@gmail.com>
2016-08-02 14:37:14 +0530

    sample

--- url_response_code.pl
+++ url_response_code.pl
@@ -3,9 +3,12 @@
 if(!($_ = shift)=~/\w/){ print "please input a file\n";exit();}
 @lines = `cat $_`;
 for $i(0 .. $#lines){
-	$_=$lines[$i];chomp;s/\w//g;defined($1) or continue;
-	$o=`curl -sL -D - $_ -o /dev/null`;
-	$url=`echo "$o"| grep Location| tail -n 1 |cut -d' ' -f2` or $url =$_;$url=~ s/\s$//g;
-	$status=` echo "$o" |grep HTTP | tail -n 1 |  cut -d' ' -f2 ` or $status="000";$status =~ s/\s$//g;
-	print $i+1 .";$status;$_;$url\n";
+	$_=$lines[$i];chomp;/\w/g;
+	if(defined($1)){
+		continue;
+	}
+		$o=`curl -sL -D - $_ -o /dev/null`;
+		$url=`echo "$o"| grep Location| tail -n 1 |cut -d' ' -f2` or $url =$_;$url=~ s/\s$//g;
+		$status=` echo "$o" |grep HTTP | tail -n 1 |  cut -d' ' -f2 ` or $status="000";$status =~ s/\s$//g;
+		print $i+1 .";$status;$_;$url\n";
 	}
